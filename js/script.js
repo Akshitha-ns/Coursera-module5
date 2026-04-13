6c977ba67f8a541a273bd991b9c736789f5c8407
Akshitha <akshithanagaraj16@gmail.com>
2026-04-13 19:06:24 +0530

    Add main JavaScript functionality for restaurant app

---
+++ js/script.js
@@ -0,0 +1,97 @@
+(function (global) {
+
+var dc = {};
+
+var homeHtmlUrl = "snippets/home-snippet.html";
+var allCategoriesUrl =
+  "https://davids-restaurant.herokuapp.com/categories.json";
+var menuItemsUrl =
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+
+function insertHtml(selector, html) {
+  document.querySelector(selector).innerHTML = html;
+}
+
+function showLoading(selector) {
+  insertHtml(selector,
+    "<div class='text-center'>Loading...</div>");
+}
+
+function insertProperty(string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  return string.replace(
+    new RegExp(propToReplace, "g"),
+    propValue
+  );
+}
+
+// STEP 0: Choose random category
+function chooseRandomCategory(categories) {
+  var randomIndex = Math.floor(Math.random() * categories.length);
+  return categories[randomIndex];
+}
+
+// Load Home Page
+dc.loadHomePage = function () {
+  showLoading("#main-content");
+
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    function (categories) {
+
+      var randomCategory =
+        chooseRandomCategory(categories);
+
+      var randomCategoryShortName =
+        "'" + randomCategory.short_name + "'";
+
+      $ajaxUtils.sendGetRequest(
+        homeHtmlUrl,
+        function (homeHtml) {
+
+          homeHtml =
+            insertProperty(
+              homeHtml,
+              "randomCategoryShortName",
+              randomCategoryShortName);
+
+          insertHtml("#main-content", homeHtml);
+        },
+        false);
+    });
+};
+
+// Load Menu Items
+dc.loadMenuItems = function (categoryShort) {
+  showLoading("#main-content");
+
+  $ajaxUtils.sendGetRequest(
+    menuItemsUrl + categoryShort,
+    function (data) {
+
+      var html =
+        "<h2>" + data.category.name + "</h2><hr>";
+
+      var items = data.menu_items;
+
+      for (var i = 0; i < items.length; i++) {
+        html += "<div><strong>" +
+          items[i].name +
+          "</strong><br>" +
+          items[i].description +
+          "</div><br>";
+      }
+
+      insertHtml("#main-content", html);
+    });
+};
+
+// Load Home on page load
+document.addEventListener("DOMContentLoaded",
+  function () {
+    dc.loadHomePage();
+  });
+
+global.$dc = dc;
+
+})(window);
